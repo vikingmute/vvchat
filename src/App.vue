86f953f025e780d15986672b894c71b19a0b01a0
Viking Zhang <vikingmute@gmail.com>
2024-07-30 17:12:48 +0800

    添加 Iconify 图标库

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <template>
   <div class=" flex items-center justify-between h-screen">
     <div class=" w-[300px] bg-gray-200 h-full border-r border-gray-300">
-      <ConversationList :items="items"/>
+      <div class="h-[90%] overflow-y-auto">
+        <ConversationList :items="items"/>
+      </div>
+      <div class="h-[10%] grid grid-cols-2 gap-2 p-2">
+        <button
+          class="shadow-sm inline-flex items-center justify-center
+            bg-green-700 text-white hover:bg-green-700/90 border border-green-700
+            h-[32px] py-[8px] px-[15px] text-sm rounded-[4px]"
+        >
+          <Icon icon="radix-icons:chat-bubble" class="mr-2"></Icon>
+          新建聊天
+        </button>
+        <button
+          class="shadow-sm inline-flex items-center justify-center
+            bg-green-50 text-green-700 hover:bg-green-700 border border-green-700 hover:text-white
+            h-[32px] py-[8px] px-[15px] text-sm rounded-[4px]"
+        >
+          <Icon icon="radix-icons:gear" class="mr-2"></Icon>
+          应用设置
+        </button>
+      </div>
     </div>
     <div class="h-full flex-1">
       content
@@ -10,13 +30,18 @@
 </template>
 
 <script setup lang="ts">
+import { Icon } from '@iconify/vue'
 import { ConversationProps } from './types'
 import ConversationList from './components/ConversationList.vue';
 const items: ConversationProps[] = [
   { id: 1, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用1', createdAt: '2024-07-03', updatedAt: '2024-07-03', providerId: 1},
   { id: 2, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用', createdAt: '2024-07-03', updatedAt: '2024-07-03', providerId: 1},
   { id: 3, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用', createdAt: '2024-07-03', updatedAt: '2024-07-03', providerId: 1},
-  { id: 4, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用, 你的说法很请正确，理解的很不错', createdAt: '2024-07-03', updatedAt: '2024-07-03', providerId: 1}
+  { id: 4, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用, 你的说法很请正确，理解的很不错', createdAt: '2024-07-03', updatedAt: '2024-07-03', providerId: 1},
+  { id: 5, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用1', createdAt: '2024-07-03', updatedAt: '2024-07-03', providerId: 1},
+  { id: 6, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用', createdAt: '2024-07-03', updatedAt: '2024-07-03', providerId: 1},
+  { id: 7, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用', createdAt: '2024-07-03', updatedAt: '2024-07-03', providerId: 1},
+  { id: 8, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用, 你的说法很请正确，理解的很不错', createdAt: '2024-07-03', updatedAt: '2024-07-03', providerId: 1}
 ]
 console.log('👋 This message is being logged by "App.vue", included via Vite');
 </script>
