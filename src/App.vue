3da7c647143479d1a9fbc7d30b5a8ae22c680133
Viking Zhang <vikingmute@gmail.com>
2024-08-01 16:19:58 +0800

    ProviderList 编码第二部分

--- src/App.vue
+++ src/App.vue
@@ -23,8 +23,10 @@
         </button>
       </div>
     </div>
-    <div class="h-full flex-1">
-      <ProviderSelect :items="providers"/>
+    <div class="h-full flex-1 flex items-center">
+      <div class="w-[80%] mx-auto">
+        <ProviderSelect :items="providers"/>
+      </div>
     </div>
   </div>
 </template>
--- src/components/ProviderSelect.vue
+++ src/components/ProviderSelect.vue
@@ -1,22 +1,37 @@
 <template>
   <div class="provider-select">
     <SelectRoot>
-      <SelectTrigger>
+      <SelectTrigger class="flex w-full items-center justify-between 
+        rounded-md py-1.5 px-3 shadow-sm border outline-none data-[placeholder]:text-gray-400">
         <SelectValue placeholder="Select a model..." />
+        <Icon
+          icon="radix-icons:chevron-down"
+          class="h-5 w-5"
+        />
       </SelectTrigger>
       <SelectPortal>
-        <SelectContent>
-          <SelectViewport>
+        <SelectContent class="bg-white rounded-md shadow-md z-[100] border">
+          <SelectViewport class="p-2">
             <div v-for="provider in items">
-              <SelectLabel>
-                <img :src="provider.avatar" :alt="provider.name">
+              <SelectLabel class="flex items-center px-6 h-7 text-gray-500">
+                <img :src="provider.avatar" :alt="provider.name" class="h-5 w-5 mr-2 rounded">
                 {{provider.name}}
               </SelectLabel>
               <SelectGroup>
-                <SelectItem v-for="(model, index) in provider.models" :key="index" :value="model">
+                <SelectItem 
+                  v-for="(model, index) in provider.models" :key="index" :value="model"
+                  class="outline-none rounded flex items-center h-7 px-6 relative
+                    text-green-700 cursor-pointer
+                    data-[highlighted]:bg-green-700 data-[highlighted]:text-white
+                  "
+                >
+                  <SelectItemIndicator class=" absolute left-2 w-6">
+                    <Icon icon="radix-icons:check" />
+                  </SelectItemIndicator>
                   <SelectItemText>{{model}}</SelectItemText>
                 </SelectItem>
               </SelectGroup>
+              <SelectSeparator class="h-[1px] my-2 bg-gray-300" />
             </div>
           </SelectViewport>
         </SelectContent>
@@ -40,6 +55,7 @@ import {
   SelectValue,
   SelectViewport,
 } from 'radix-vue'
+import { Icon } from '@iconify/vue'
 import { ProviderProps } from '../types'
 defineProps<{ items: ProviderProps[] }>()
 </script>
