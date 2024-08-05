78b1f044f30e28bb191b88ef2c0f57098024379d
Viking Zhang <vikingmute@gmail.com>
2024-08-05 09:40:10 +0800

    添加 MessageList 组件

--- src/components/ConversationList.vue
+++ src/components/ConversationList.vue
@@ -5,13 +5,13 @@
       v-for="item in items"
       :key="item.id"
     >
-     <a href="#">
+     <RouterLink to="/conversation">
       <div class=" flex justify-between items-center text-sm leading-5 text-gray-500">
         <span>{{item.selectedModel}}</span>
         <span>{{item.updatedAt}}</span>
       </div>
       <h2 class=" font-semibold leading-6 text-gray-900 truncate">{{item.title}}</h2>
-     </a>
+     </RouterLink>
     </div>
   </div>
 </template>
