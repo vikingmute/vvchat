d8278e52c7efc043cc47f67d97aef4ed6aa0a2ef
Viking Zhang <vikingmute@gmail.com>
2024-08-05 17:17:53 +0800

    完成小重构并且解决数据响应式问题

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class=" flex items-center justify-between h-screen">
     <div class=" w-[300px] bg-gray-200 h-full border-r border-gray-300">
       <div class="h-[90%] overflow-y-auto">
-        <ConversationList :items="items"/>
+        <ConversationList :items="conversations"/>
       </div>
       <div class="h-[10%] grid grid-cols-2 gap-2 p-2">
         <RouterLink to="/">
@@ -34,43 +34,9 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
 import { Icon } from '@iconify/vue'
-import { ConversationProps, ProviderProps } from './types'
+import { conversations } from './testData'
 import ConversationList from './components/ConversationList.vue'
-import ProviderSelect from './components/ProviderSelect.vue'
-import MessageInput from './components/MessageInput.vue'
-const selectedModel = ref('')
-const items: ConversationProps[] = [
-  { id: 1, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用1', createdAt: '2024-07-03', updatedAt: '2024-07-03', providerId: 1},
-  { id: 2, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用', createdAt: '2024-07-03', updatedAt: '2024-07-03', providerId: 1},
-  { id: 3, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用', createdAt: '2024-07-03', updatedAt: '2024-07-03', providerId: 1},
-  { id: 4, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用, 你的说法很请正确，理解的很不错', createdAt: '2024-07-03', updatedAt: '2024-07-03', providerId: 1},
-  { id: 5, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用1', createdAt: '2024-07-03', updatedAt: '2024-07-03', providerId: 1},
-  { id: 6, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用', createdAt: '2024-07-03', updatedAt: '2024-07-03', providerId: 1},
-  { id: 7, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用', createdAt: '2024-07-03', updatedAt: '2024-07-03', providerId: 1},
-  { id: 8, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用, 你的说法很请正确，理解的很不错', createdAt: '2024-07-03', updatedAt: '2024-07-03', providerId: 1}
-]
-const providers: ProviderProps[] = [
-  {     
-    id: 1,
-    name: '文心一言', 
-    desc: '文心一言 百度出品的大模型',
-    models: ['ERNIE-4.0-8K', 'ERNIE-3.5-8K', 'ERNIE-Speed-8K'],
-    avatar: 'https://qph.cf2.poecdn.net/main-thumb-pb-3004-50-jougqzjtwfqfyqprxbdwofvnwattmtrg.jpeg',
-    createdAt: '2024-07-03',
-    updatedAt: '2024-07-03'
-  },
-  {     
-    id: 2,
-    name: '通义千问', 
-    desc: '通义千问',
-    // https://help.aliyun.com/zh/dashscope/developer-reference/api-details?spm=a2c4g.11186623.0.0.5bf41507xgULX5#b148acc634pfc
-    models: ['qwen-turbo', 'qwen-plus', 'qwen-max'],
-    avatar: 'https://qph.cf2.poecdn.net/main-thumb-pb-3004-50-jougqzjtwfqfyqprxbdwofvnwattmtrg.jpeg',
-    createdAt: '2024-07-03',
-    updatedAt: '2024-07-03'
-  }
-]
+
 console.log('👋 This message is being logged by "App.vue", included via Vite');
 </script>
--- src/views/Conversation.vue
+++ src/views/Conversation.vue
@@ -1,6 +1,9 @@
 <template>
-  {{route}}
-<div class="w-[80%] mx-auto h-[85%] overflow-y-auto pt-2">
+<div class="h-[10%] bg-gray-200 border-b border-gray-300 flex items-center px-3 justify-between" v-if="convsersation">
+  <h3 class="font-semibold  text-gray-900">{{convsersation.title}}</h3>
+  <span class="text-sm text-gray-500">{{convsersation.updatedAt}}</span>
+</div>
+<div class="w-[80%] mx-auto h-[75%] overflow-y-auto pt-2">
   <MessageList :messages="filteredMessages"/>
 </div>
 <div class="w-[80%] mx-auto h-[15%] flex items-center">
@@ -8,25 +11,22 @@
 </div>
 </template>
 <script lang="ts" setup>
+import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import MessageInput from '../components/MessageInput.vue'
-import MessageList from '../components/MessageList.vue';
-import { MessageProps } from '../types'
+import MessageList from '../components/MessageList.vue'
+import { MessageProps, ConversationProps } from '../types'
+import { messages, conversations } from '../testData'
 const route = useRoute()
-const conversationId = parseInt(route.params.id as string)
-const messages: MessageProps[] = [
-  { id: 1, content: '什么是光合作用', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'question', conversationId: 1 },
-  { id: 2, content: '你的说法很请正确，理解的很不错,你的说法很请正确，理解的很不错', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'answer', conversationId: 1 },
-  { id: 3, content: '请告诉我更多', createdAt: '2024-07-03',updatedAt: '2024-07-03',  type: 'question', conversationId: 1 },
-  { id: 4, content: '你的说法很请正确，理解的很不错,你的说法很请正确，理解的很不错', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'answer', conversationId: 1 },
-  { id: 5, content: '还有更多的信息吗', createdAt: '2024-07-03', type: 'question', updatedAt: '2024-07-03', conversationId: 1 },
-  { id: 6, content: '', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'answer', status: 'loading', conversationId: 1 },
-  { id: 7, content: '什么是光合作用', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'question', conversationId: 2 },
-  { id: 8, content: '你的说法很请正确，理解的很不错,你的说法很请正确，理解的很不错', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'answer', conversationId: 2 },
-  { id: 9, content: '请告诉我更多', createdAt: '2024-07-03',updatedAt: '2024-07-03',  type: 'question', conversationId: 2 },
-  { id: 10, content: '你的说法很请正确，理解的很不错,你的说法很请正确，理解的很不错', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'answer', conversationId: 2 },
-  { id: 11, content: '还有更多的信息吗', createdAt: '2024-07-03', type: 'question', updatedAt: '2024-07-03', conversationId: 3 },
-  { id: 12, content: '', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'answer', status: 'loading', conversationId: 3 },
-]
-const filteredMessages = messages.filter(message => message.conversationId === conversationId)
+const filteredMessages = ref<MessageProps[]>([])
+const convsersation = ref<ConversationProps>()
+let conversationId = parseInt(route.params.id as string)
+filteredMessages.value = messages.filter(message => message.conversationId === conversationId)
+convsersation.value = conversations.find(item => item.id === conversationId)
+
+watch(() => route.params.id, (newId: string) => {
+  conversationId = parseInt(newId)
+  filteredMessages.value = messages.filter(message => message.conversationId === conversationId)
+  convsersation.value = conversations.find(item => item.id === conversationId)
+})
 </script>
