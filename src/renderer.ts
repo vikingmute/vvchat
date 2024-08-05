0b1ab09ed83da47dd1f5859717f4c3218d2f10f7
Viking Zhang <vikingmute@gmail.com>
2024-08-05 15:28:12 +0800

    学习使用 useRouter 和 useRoute 钩子函数

--- src/renderer.ts
+++ src/renderer.ts
@@ -35,7 +35,7 @@ import './index.css';
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/conversation', component: Conversation },
+  { path: '/conversation/:id', component: Conversation },
   { path: '/settings', component: Settings }
 ]
 const router = createRouter({
--- src/views/Conversation.vue
+++ src/views/Conversation.vue
@@ -1,15 +1,19 @@
 <template>
+  {{route}}
 <div class="w-[80%] mx-auto h-[85%] overflow-y-auto pt-2">
-  <MessageList :messages="messages"/>
+  <MessageList :messages="filteredMessages"/>
 </div>
 <div class="w-[80%] mx-auto h-[15%] flex items-center">
   <MessageInput  />
 </div>
 </template>
 <script lang="ts" setup>
+import { useRoute } from 'vue-router'
 import MessageInput from '../components/MessageInput.vue'
 import MessageList from '../components/MessageList.vue';
 import { MessageProps } from '../types'
+const route = useRoute()
+const conversationId = parseInt(route.params.id as string)
 const messages: MessageProps[] = [
   { id: 1, content: '什么是光合作用', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'question', conversationId: 1 },
   { id: 2, content: '你的说法很请正确，理解的很不错,你的说法很请正确，理解的很不错', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'answer', conversationId: 1 },
@@ -17,5 +21,12 @@ const messages: MessageProps[] = [
   { id: 4, content: '你的说法很请正确，理解的很不错,你的说法很请正确，理解的很不错', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'answer', conversationId: 1 },
   { id: 5, content: '还有更多的信息吗', createdAt: '2024-07-03', type: 'question', updatedAt: '2024-07-03', conversationId: 1 },
   { id: 6, content: '', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'answer', status: 'loading', conversationId: 1 },
+  { id: 7, content: '什么是光合作用', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'question', conversationId: 2 },
+  { id: 8, content: '你的说法很请正确，理解的很不错,你的说法很请正确，理解的很不错', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'answer', conversationId: 2 },
+  { id: 9, content: '请告诉我更多', createdAt: '2024-07-03',updatedAt: '2024-07-03',  type: 'question', conversationId: 2 },
+  { id: 10, content: '你的说法很请正确，理解的很不错,你的说法很请正确，理解的很不错', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'answer', conversationId: 2 },
+  { id: 11, content: '还有更多的信息吗', createdAt: '2024-07-03', type: 'question', updatedAt: '2024-07-03', conversationId: 3 },
+  { id: 12, content: '', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'answer', status: 'loading', conversationId: 3 },
 ]
+const filteredMessages = messages.filter(message => message.conversationId === conversationId)
 </script>
