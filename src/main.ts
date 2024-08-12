51b5b7ce159d75758611af7aa3c54b61a878e032
Viking Zhang <vikingmute@gmail.com>
2024-08-12 17:35:03 +0800

    试用阿里通义千问大模型 VL 识图

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { ChatCompletion } from '@baiducloud/qianfan'
 import path from 'path'
 import 'dotenv/config'
 import OpenAI from 'openai'
+import fs from 'fs/promises'
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -32,13 +33,26 @@ const createWindow = async () => {
     apiKey: process.env['ALI_API_KEY'],
     baseURL: 'https://dashscope.aliyuncs.com/compatible-mode/v1'
   })
+  const imageBuffer = await fs.readFile('/Users/viking/Desktop/dog.jpeg')
+  const base64Image = imageBuffer.toString('base64')
+  console.log('base64', base64Image)
   const resp = await client.chat.completions.create({
-    messages: [
-      { role: 'system', content: '你现在是一只卡通片里面的可爱小狗，请模仿汪汪队长的口吻进行回答' },
-      { role: 'user', content: '请问队长，老鼠为什么有害呢？' }
-    ],
-    model: 'qwen-turbo',
+    messages: [ {
+      role: 'user',
+      content: [
+        { type: 'text', text: '图中是什么动物？'},
+        { type: 'image_url', image_url: { url: `data:image/jpeg;base64,${base64Image}`}}
+      ]
+    }],
+    model: 'qwen-vl-plus'
   })
+  // const resp = await client.chat.completions.create({
+  //   messages: [
+  //     { role: 'system', content: '你现在是一只卡通片里面的可爱小狗，请模仿汪汪队长的口吻进行回答' },
+  //     { role: 'user', content: '请问队长，老鼠为什么有害呢？' }
+  //   ],
+  //   model: 'qwen-turbo',
+  // })
   // for await (const chunk of stream) {
   //   console.log(chunk.choices[0].delta)
   // }
