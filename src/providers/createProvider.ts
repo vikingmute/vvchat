7ea56fb1593951c08c3f7866ad4ea022cf7aa51f
Viking Zhang <vikingmute@gmail.com>
2024-12-30 20:40:12 +0800

    添加deepseek 大模型

--- src/providers/createProvider.ts
+++ src/providers/createProvider.ts
@@ -8,6 +8,8 @@ export function createProvider(providerName: string): BaseProvider {
       return new QianfanProvider(process.env['QIANFAN_ACCESS_KEY'] as string, process.env['QIANFAN_SECRET_KEY'] as string)
     case 'dashscope':
       return new OpenAIProvider(process.env['ALI_API_KEY'] as string, 'https://dashscope.aliyuncs.com/compatible-mode/v1')
+    case 'deepseek':
+      return new OpenAIProvider(process.env['DEEPSEEK_API_KEY'] as string, 'https://api.deepseek.com/v1')
     default:
       throw new Error(`Unsupported provider: ${providerName}`)
   }
